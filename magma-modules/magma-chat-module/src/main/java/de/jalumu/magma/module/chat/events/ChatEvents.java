4423ef4ab3b0bb9d8197386a0958a4339ce43fc6
Jan-Luca Musold <musoldjl@gmail.com>
2022-10-03 23:46:51 +0200

    Working chat system

--- magma-modules/magma-chat-module/src/main/java/de/jalumu/magma/module/chat/events/ChatEvents.java
+++ magma-modules/magma-chat-module/src/main/java/de/jalumu/magma/module/chat/events/ChatEvents.java
@@ -1,6 +1,13 @@
 package de.jalumu.magma.module.chat.events;
 
 import de.jalumu.magma.platform.base.module.MagmaModule;
+import de.jalumu.magma.platform.base.text.placeholder.Placeholders;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -9,13 +16,18 @@ public class ChatEvents implements Listener {
 
     private MagmaModule module;
 
-    public ChatEvents(MagmaModule module){
+    public ChatEvents(MagmaModule module) {
         this.module = module;
     }
 
     @EventHandler
-    public void onChat(AsyncPlayerChatEvent event) {
-        //event.setMessage("ASDF");
+    public void onChat(AsyncChatEvent event) {
+        Player player = event.getPlayer();
+        Component chatFormat = MiniMessage.miniMessage().deserialize("<rank_displayname> <dark_gray>| <player_first_prefix_color><player_name> <dark_gray>-<reset> <message>", Placeholders.player(player.getUniqueId()), Placeholder.component("message", event.message()));
+        for (Player p : Bukkit.getOnlinePlayers()){
+            p.sendMessage(chatFormat);
+        }
+        event.setCancelled(true);
     }
 
 }
