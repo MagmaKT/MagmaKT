dd5d6fcc657ea61f238f50885671addc104a0036
Jan-Luca Musold <musoldjl@gmail.com>
2022-10-29 23:24:11 +0200

    Added Bukkit Command Annotation

---
+++ magma-annotations/magma-annotations-bukkit-platform/src/main/kotlin/de/jalumu/magma/annotation/bukkit/platform/permission/PermissionDefault.kt
@@ -0,0 +1,10 @@
+package de.jalumu.magma.annotation.bukkit.platform.permission
+
+enum class PermissionDefault(val value: String) {
+
+    GRANTED("true"),
+    DENIED("false"),
+    OP("op"),
+    NOT_OP("not op");
+
+}
