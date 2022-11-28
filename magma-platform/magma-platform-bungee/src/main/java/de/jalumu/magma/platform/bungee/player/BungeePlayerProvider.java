d70acc2d7e6a8892d0b33ba5ec216935e9a50ba4
Jan-Luca Musold <musoldjl@gmail.com>
2022-11-28 23:54:23 +0100

    bungeecord player implementation

--- magma-platform/magma-platform-bungee/src/main/java/de/jalumu/magma/platform/bungee/player/BungeePlayerProvider.java
+++ magma-platform/magma-platform-bungee/src/main/java/de/jalumu/magma/platform/bungee/player/BungeePlayerProvider.java
@@ -1,37 +1,35 @@
 package de.jalumu.magma.platform.bungee.player;
 
 import de.jalumu.magma.platform.base.player.BasePlayerProvider;
-import de.jalumu.magma.player.FetchedPlayer;
+import de.jalumu.magma.platform.bungee.bootstrap.MagmaBungeeBootstrap;
 import de.jalumu.magma.player.MagmaPlayer;
-import de.jalumu.magma.player.PlayerProvider;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 import java.util.UUID;
 
 public class BungeePlayerProvider extends BasePlayerProvider {
 
-    @Override
-    protected FetchedPlayer getCachedPlayer(String name) {
-        return fetchPlayer(name);
-    }
+    private MagmaBungeeBootstrap bootstrap;
 
-    @Override
-    protected FetchedPlayer getCachedPlayer(UUID uuid) {
-        return fetchPlayer(uuid);
+    public BungeePlayerProvider(MagmaBungeeBootstrap bootstrap) {
+        this.bootstrap = bootstrap;
     }
 
     @Override
     protected MagmaPlayer getOnlinePlayer(String name) {
-        return null;
+        ProxiedPlayer player = ProxyServer.getInstance().getPlayer(name);
+        return new BungeePlayer(bootstrap, player);
     }
 
     @Override
     protected MagmaPlayer getOnlinePlayer(UUID uuid) {
-        return null;
+        ProxiedPlayer player = ProxyServer.getInstance().getPlayer(uuid);
+        return new BungeePlayer(bootstrap, player);
     }
 
     @Override
     protected MagmaPlayer[] getOnlinePlayers() {
-        return new MagmaPlayer[0];
+        return ProxyServer.getInstance().getPlayers().stream().map(player -> new BungeePlayer(bootstrap, player)).toArray(MagmaPlayer[]::new);
     }
 }
