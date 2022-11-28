95bee3e72c79fbbe3d8a684e869cef369006f8fc
Jan-Luca Musold <musoldjl@gmail.com>
2022-11-29 00:00:41 +0100

    bukkit player implementation

--- magma-platform/magma-platform-bukkit/src/main/java/de/jalumu/magma/platform/bukkit/player/BukkitPlayerProvider.java
+++ magma-platform/magma-platform-bukkit/src/main/java/de/jalumu/magma/platform/bukkit/player/BukkitPlayerProvider.java
@@ -2,42 +2,33 @@ package de.jalumu.magma.platform.bukkit.player;
 
 import de.jalumu.magma.platform.base.player.BasePlayerProvider;
 import de.jalumu.magma.platform.bukkit.bootstrap.MagmaBukkitBootstrap;
-import de.jalumu.magma.player.FetchedPlayer;
 import de.jalumu.magma.player.MagmaPlayer;
-import de.jalumu.magma.player.PlayerProvider;
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
 public class BukkitPlayerProvider extends BasePlayerProvider {
 
+    private MagmaBukkitBootstrap magma;
+
     public BukkitPlayerProvider(MagmaBukkitBootstrap bukkitBootstrap) {
-        super();
-    }
-
-    @Override
-    protected FetchedPlayer getCachedPlayer(String name) {
-        return fetchPlayer(name);
-    }
-
-    @Override
-    protected FetchedPlayer getCachedPlayer(UUID uuid) {
-        return fetchPlayer(uuid);
+        magma = bukkitBootstrap;
     }
 
     @Override
     protected MagmaPlayer getOnlinePlayer(String name) {
-        return null;
+        Player player = magma.getServer().getPlayer(name);
+        return new BukkitPlayer(magma, player);
     }
 
     @Override
     protected MagmaPlayer getOnlinePlayer(UUID uuid) {
-        return null;
+        Player player = magma.getServer().getPlayer(uuid);
+        return new BukkitPlayer(magma, player);
     }
 
     @Override
     protected MagmaPlayer[] getOnlinePlayers() {
-        return new MagmaPlayer[0];
+        return magma.getServer().getOnlinePlayers().stream().map(player -> new BukkitPlayer(magma, player)).toArray(MagmaPlayer[]::new);
     }
 }
