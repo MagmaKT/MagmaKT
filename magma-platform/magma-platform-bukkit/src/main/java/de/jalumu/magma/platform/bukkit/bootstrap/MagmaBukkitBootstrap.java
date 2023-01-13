aefb748f0a667e10ccfa43300f4bbbf334641eb5
Jan-Luca Musold <musoldjl@gmail.com>
2023-01-14 00:45:05 +0100

    Removed VaultApi

--- magma-platform/magma-platform-bukkit/src/main/java/de/jalumu/magma/platform/bukkit/bootstrap/MagmaBukkitBootstrap.java
+++ magma-platform/magma-platform-bukkit/src/main/java/de/jalumu/magma/platform/bukkit/bootstrap/MagmaBukkitBootstrap.java
@@ -3,9 +3,6 @@ package de.jalumu.magma.platform.bukkit.bootstrap;
 import de.exlll.configlib.YamlConfigurations;
 import de.jalumu.magma.annotation.bukkit.platform.application.BukkitPlugin;
 import de.jalumu.magma.platform.base.config.ServerIdConfig;
-import de.jalumu.magma.platform.base.text.TextHandler;
-import de.jalumu.magma.text.Text;
-import de.jalumu.magma.util.sandbox.Sandbox;
 import de.jalumu.magma.platform.MagmaPlatform;
 import de.jalumu.magma.platform.MagmaPlatformType;
 import de.jalumu.magma.platform.ServerImplementation;
@@ -22,11 +19,7 @@ import de.jalumu.magma.platform.bukkit.text.BukkitNotificationProvider;
 import de.jalumu.magma.platform.bukkit.text.placeholder.BukkitPlaceholderProvider;
 import de.jalumu.magma.text.notification.NotificationProvider;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
-import net.kyori.adventure.text.Component;
-import net.milkbowl.vault.permission.Permission;
 import org.bstats.bukkit.Metrics;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import revxrsal.commands.CommandHandler;
 import revxrsal.commands.bukkit.BukkitCommandHandler;
@@ -36,14 +29,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-@BukkitPlugin(name = "MagmaKT-Bukkit", version = "Dev-Build", description = "MagmaKT for Bukkit", author = "JaLuMu", dependsPlugin = {}, softDependsPlugin = {"ProtocolLib", "Vault", "LuckPerms"})
+@BukkitPlugin(name = "MagmaKT-Bukkit", version = "Dev-Build", description = "MagmaKT for Bukkit", author = "JaLuMu", dependsPlugin = {}, softDependsPlugin = {"ProtocolLib", "LuckPerms"})
 public class MagmaBukkitBootstrap extends JavaPlugin implements MagmaPlatform {
 
     private BukkitAudiences adventure;
 
     private ModuleLoader moduleLoader;
-
-    private Permission perms = null;
 
     private BukkitMySQLManager mySQLManager;
 
@@ -70,8 +61,6 @@ public class MagmaBukkitBootstrap extends JavaPlugin implements MagmaPlatform {
     @Override
     public void onEnable() {
         Metrics metrics = new Metrics(this, 16417);
-
-        setupPermissions();
 
         File serverIdFile = new File(getDataFolder(), "serverID.yml");
 
@@ -152,22 +141,5 @@ public class MagmaBukkitBootstrap extends JavaPlugin implements MagmaPlatform {
     @Override
     public Object getMagmaPluginInstance() {
         return this;
-    }
-
-
-    private boolean setupPermissions() {
-        if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
-            return false;
-        }
-        RegisteredServiceProvider<Permission> rsp = Bukkit.getServicesManager().getRegistration(Permission.class);
-        if (rsp == null) {
-            return false;
-        }
-        perms = rsp.getProvider();
-        return perms != null;
-    }
-
-    public Permission getPerms() {
-        return perms;
     }
 }
