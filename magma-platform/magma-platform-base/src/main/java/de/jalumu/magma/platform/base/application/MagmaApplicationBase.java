2d57ccebc15aba0da9eefdbf3d8af2dcb2a390a3
Jan-Luca Musold <jalumu@jalumu.dev>
2023-04-29 20:00:19 +0200

    Updated BukkitApplication

--- magma-platform/magma-platform-base/src/main/java/de/jalumu/magma/platform/base/application/MagmaApplicationBase.java
+++ magma-platform/magma-platform-base/src/main/java/de/jalumu/magma/platform/base/application/MagmaApplicationBase.java
@@ -1,9 +1,12 @@
 package de.jalumu.magma.platform.base.application;
 
+import revxrsal.commands.CommandHandler;
+
 public interface MagmaApplicationBase {
 
     void initialize();
     void start();
     void shutdown();
+    CommandHandler getCommandHandler();
 
 }
--- magma-platform/magma-platform-bukkit/src/main/java/de/jalumu/magma/platform/bukkit/application/BukkitApplication.java
+++ magma-platform/magma-platform-bukkit/src/main/java/de/jalumu/magma/platform/bukkit/application/BukkitApplication.java
@@ -1,11 +1,20 @@
 package de.jalumu.magma.platform.bukkit.application;
 
+import de.exlll.configlib.ConfigLib;
+import de.exlll.configlib.YamlConfigurationProperties;
 import de.jalumu.magma.platform.base.application.MagmaApplicationBase;
+import de.jalumu.magma.platform.bukkit.command.MagmaBukkitCommandAnnotationReplacer;
 import org.bukkit.plugin.java.JavaPlugin;
+import revxrsal.commands.CommandHandler;
+import revxrsal.commands.bukkit.BukkitCommandHandler;
 
 import java.io.File;
 
 public abstract class BukkitApplication extends JavaPlugin implements MagmaApplicationBase {
+
+    private CommandHandler commandHandler;
+
+    private YamlConfigurationProperties.Builder configurationProperties;
 
     @Override
     public void onLoad() {
@@ -21,11 +30,25 @@ public abstract class BukkitApplication extends JavaPlugin implements MagmaAppli
 
     @Override
     public void onEnable() {
+
+        configurationProperties = ConfigLib.BUKKIT_DEFAULT_PROPERTIES.toBuilder();
+
+        commandHandler = BukkitCommandHandler.create(this);
+        commandHandler.registerAnnotationReplacer(de.jalumu.magma.command.MagmaCommand.class, new MagmaBukkitCommandAnnotationReplacer());
         start();
     }
 
     @Override
     public void onDisable() {
         shutdown();
+    }
+
+    @Override
+    public CommandHandler getCommandHandler() {
+        return commandHandler;
+    }
+
+    public YamlConfigurationProperties.Builder getConfigurationProperties() {
+        return configurationProperties;
     }
 }
