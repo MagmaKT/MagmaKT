e23841abf3c8bae9e82610eb81b0ccf72274d928
Jan-Luca Musold <jalumu@jalumu.dev>
2023-06-08 17:46:55 +0200

    Added BungeeApplication

---
+++ magma-platform/magma-platform-bungee/src/main/java/de/jalumu/magma/platform/bungee/command/MagmaBungeeCommandAnnotationReplacer.java
@@ -0,0 +1,50 @@
+package de.jalumu.magma.platform.bungee.command;
+
+
+import de.jalumu.magma.command.MagmaCommand;
+import org.jetbrains.annotations.NotNull;
+import revxrsal.commands.annotation.AutoComplete;
+import revxrsal.commands.annotation.Command;
+import revxrsal.commands.annotation.Description;
+import revxrsal.commands.annotation.Usage;
+import revxrsal.commands.annotation.dynamic.AnnotationReplacer;
+import revxrsal.commands.annotation.dynamic.Annotations;
+import revxrsal.commands.bungee.annotation.CommandPermission;
+
+import javax.annotation.Nullable;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class MagmaBungeeCommandAnnotationReplacer implements AnnotationReplacer<MagmaCommand> {
+
+    @Override
+    public @Nullable
+    Collection<Annotation> replaceAnnotations(@NotNull AnnotatedElement element, @NotNull MagmaCommand annotation) {
+        System.out.println("Translating " + annotation.command());
+        Command command = Annotations.create(Command.class, "value", annotation.command());
+        Usage usage = Annotations.create(Usage.class, "value", annotation.usage());
+        Description description = Annotations.create(Description.class, "value", annotation.description());
+        CommandPermission permission = Annotations.create(CommandPermission.class, "value", annotation.permission());
+
+        if (!annotation.autoComplete().equals("")) {
+            AutoComplete autoComplete = Annotations.create(AutoComplete.class, "value", annotation.autoComplete());
+            return Arrays.asList(
+                    command,
+                    usage,
+                    description,
+                    permission,
+                    autoComplete
+            );
+        }
+
+        return Arrays.asList(
+                command,
+                usage,
+                description,
+                permission
+        );
+    }
+
+}
